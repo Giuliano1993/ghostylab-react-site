0bcee0e280131a28c29ee887fe1cd430ebda57e4
Giuliano Gostinfini <giuliano.gostinfini.93@gmail.com>
2023-03-04 20:25:52 +0100

    [ add ] terminal button [ add  ] contact basic page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Contact = ()=>{
+  const [formData, setFormData] = useState({
+    name: '',
+    mail: '',
+    message: ''
+  })
+
+  const handleData = e =>{
+    setFormData({
+      ...formData,
+      [e.target.name]:e.target.value
+    })
+  }
   return(
 
-    <div className="container">Contact</div>
+    <div className="container" id="contacts">
+      <form>
+        <div>
+          <div>
+            <label htmlFor="namet">Name: </label>
+            <input type="text" name="name" onChange={handleData} value={formData.name} className="form-control"/>
+          </div>
+          <div>
+            <label htmlFor="mail">Mail: </label>
+            <input type="mail" name="mail" onChange={handleData} value={formData.mail} className="form-control"/>
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" onChange={handleData} value={formData.message} className="form-control"></textarea>
+          </div>
+          <div>
+            <button type="submit">Send</button>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
