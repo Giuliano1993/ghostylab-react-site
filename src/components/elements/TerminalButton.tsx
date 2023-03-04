cff05e7cc05896349d6de49f1a845853e0f83942
Giuliano Gostinfini <giuliano.gostinfini.93@gmail.com>
2023-03-04 20:35:01 +0100

    [fix] breakpoint and buttons

--- src/components/elements/TerminalButton.tsx
+++ src/components/elements/TerminalButton.tsx
@@ -4,13 +4,13 @@ import React from "react";
 export default function TerminalButton({href,text}){
   return(
     <NavLink className="navbar-item" to={href} >
-      <div>+--------------------------+</div>
+      <div>+-------<span className="d-lg-none" >--</span>-------------------+</div>
       <div>
         <p className="side">|</p>
         <p>{text}<span className="pointer"></span></p>
         <p className="side">|</p>
       </div>
-      <div>+--------------------------+</div>
+      <div>+-----------<span className="d-lg-none" >--</span>---------------+</div>
     </NavLink>
   );
 }
